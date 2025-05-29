0750d81e2e16fe32dbc8cba94c5a0606080fc6a0
HoChoRoo <rttkhc0304@gmail.com>
2025-05-29 11:45:38 +0900

    ✨ feat: [BVFH-116] 공통컴포넌트(버튼-사이즈 추가, filled-input 생성)

--- src/components/ui/filled-input.tsx
+++ src/components/ui/filled-input.tsx
@@ -2,27 +2,42 @@
 
 import { cn } from '@/lib/cn';
 import type React from 'react';
-
 import { forwardRef } from 'react';
 
 interface FilledInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   _variant?: 'default' | 'gray-bg' | 'bordered';
+  label?: string;
+  error?: string;
 }
 
 export const FilledInput = forwardRef<HTMLInputElement, FilledInputProps>(
-  ({ className, _variant = 'default', disabled, ...props }, ref) => {
+  (
+    { className, _variant = 'default', disabled, label, error, ...props },
+    ref,
+  ) => {
     return (
-      <input
-        ref={ref}
-        className={cn(
-          'flex-1 bg-transparent focus:outline-none text-base placeholder:text-blue-100',
-          disabled && 'cursor-not-allowed',
-          className,
-        )}
-        disabled={disabled}
-        {...props}
-      />
+      <>
+        <div
+          className={cn(
+            'w-full space-y-2 bg-gray-200 rounded-full p-3.5',
+            className,
+          )}
+        >
+          <input
+            placeholder={label}
+            ref={ref}
+            className={cn(
+              'flex-1  ml-3 focus:outline-none text-base text-blue-100 placeholder:text-blue-100',
+              disabled && 'cursor-not-allowed',
+            )}
+            disabled={disabled}
+            {...props}
+          />
+        </div>
+        {error && <p className='text-sm text-red-500 ml-4'>{error}</p>}
+      </>
     );
   },
 );
+
 FilledInput.displayName = 'FilledInput';

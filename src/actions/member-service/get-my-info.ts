cf171e262db37c92383c342b40a4ea34a5ff5225
HoChoRoo <rttkhc0304@gmail.com>
2025-06-18 15:13:44 +0900

    ✨ feat: [BVFH-280] mainLayout에 template 설정
    페이지 이동시 전환 효과 - fade-in 효과

---
+++ src/actions/member-service/get-my-info.ts
@@ -0,0 +1,29 @@
+'use server';
+
+import { cookies } from 'next/headers';
+import { instance } from '../instance';
+import { MemberInfo } from '@/types/member';
+import { ErrorResponse } from '@/types/api';
+
+export async function getMyInfo(): Promise<MemberInfo | ErrorResponse> {
+  const cookieStore = await cookies();
+  const memberUuid = cookieStore.get('memberUuid')?.value;
+  if (!memberUuid) {
+    return {
+      code: '401',
+      message: 'Member UUID is not found',
+    } as ErrorResponse;
+  }
+  try {
+    const response = await instance.get<MemberInfo>(
+      `/member-service/api/v1/member/${memberUuid}`,
+      {
+        requireAuth: true,
+        cache: 'no-cache',
+      },
+    );
+    return response;
+  } catch (error) {
+    return error as ErrorResponse;
+  }
+}

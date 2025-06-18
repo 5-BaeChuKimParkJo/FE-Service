90878d9e18229c09a4ccb4d589fe5b416a342f64
HoChoRoo <rttkhc0304@gmail.com>
2025-06-18 15:16:00 +0900

    ✨ feat: [BVFH-280] 메인 페이지 - 회원 정보 섹션
    프로필이미지, 닉네임, 포인트, 등급

---
+++ src/components/members/MemberInfoSection.tsx
@@ -0,0 +1,50 @@
+import { ErrorResponse } from '@/types/api';
+import { MemberInfo } from '@/types/member';
+import { isErrorResponse } from '@/utils/type-guards';
+import { MemberInfoSectionSkeleton } from './MemberInfoSectionSkeleton';
+import Image from 'next/image';
+
+export function MemberInfoSection({
+  memberInfo,
+}: {
+  memberInfo: MemberInfo | ErrorResponse;
+}) {
+  if (isErrorResponse(memberInfo)) {
+    return <MemberInfoSectionSkeleton />;
+  }
+
+  return (
+    <section className='flex flex-col gap-4 py-2'>
+      <div className='flex items-center gap-4'>
+        <Image
+          src={memberInfo.profileImageUrl || '/images/grade/grade.png'}
+          alt='profile'
+          width={40}
+          height={40}
+          className='rounded-full'
+        />
+        <div className='flex flex-col'>
+          <p className='text-sm text-gray-500'>찰낙찰낙 쏟아지는 경매</p>
+          <p className='text-lg font-bold'>
+            {memberInfo.nickname || '유저 닉네임'}
+          </p>
+        </div>
+        <div className='ml-auto mr-2 flex items-center '>
+          <div className='flex items-center bg-primary-200 rounded-full  h-10 min-w-[80px] justify-center'>
+            <div className='relative w-7 h-7 rounded-full bg-white flex items-center justify-center mr-4'>
+              <Image
+                src='/images/grade/grade.png'
+                alt='coin'
+                fill
+                style={{ objectFit: 'cover' }}
+              />
+            </div>
+            <span className='text-white text-base font-bold'>
+              {memberInfo.point}
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
